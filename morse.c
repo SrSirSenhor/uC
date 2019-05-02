072059774920fad5bc235dcc1a06076a08373fd1
SrSirSenhor <50216006+SrSirSenhor@users.noreply.github.com>
2019-05-02 15:57:37 -0300

    Update morse.c
    OBS: Feito na IDE do Arduino

--- morse.c
+++ morse.c
@@ -1,37 +1,186 @@
-#define LED     12
-#define LONGO   800
-#define CURTO   400
-#define INTERV1 200
+#define LED 10
+#define LONGO 600
+#define CURTO 200
+#define intervalo 100
 
-void longo() {
-  digitalWrite(LED, HIGH); delay(LONGO);
-  digitalWrite(LED, LOW);  delay(INTERV1);
-}
 
 void curto() {
   digitalWrite(LED, HIGH); delay(CURTO);
-  digitalWrite(LED, LOW);  delay(INTERV1);
+  digitalWrite(LED, LOW);  delay(intervalo);
+}
+ 
+void longo() {
+  digitalWrite(LED, HIGH); delay(LONGO);
+  digitalWrite(LED, LOW);  delay(intervalo);
 }
 
-void morse(char palavra[]) {
-  for(byte i=0; palavra[i]!='\0'; i++)
-    switch (toupper(palavra[i])) {
-     case 'A': curto(); longo(); break;
-     case 'B': longo(); curto(); curto(); curto(); break;
-     case 'C': longo(); curto(); longo(); curto(); break;
-     case 'O': longo(); longo(); longo(); break;
-     case 'S': curto(); curto(); curto(); break;
-     default: for(byte i=0; i<5; i++) curto();
-    }
+void espacoPalavras(){
+  delay(intervalo*3);
 }
+
+void espacoFrases(){
+  delay(intervalo*10);
+}
+
+void morse(char palavra[]){
+  for(byte i=0; palavra[i] != '/0'; i++)
+    switch(toupper(palavra[i])){
+      case 'A':
+        curto(); longo();
+        break;
+      
+      case 'B':
+        longo(); curto(); curto(); curto();
+        break;
+      
+      case 'C':
+        longo(); curto(); longo(); curto();
+        break;
+      
+      case 'D':
+        longo(); curto(); curto();
+        break;
+      
+      case 'E':
+        curto();
+        break;
+      
+      case 'F':
+        curto(); curto(); longo(); curto();
+        break;
+      
+      case 'G':
+        longo(); longo(); curto();
+        break;
+      
+      case 'H':
+        curto(); curto(); curto(); curto();
+        break;
+      
+      case 'I':
+        curto(); curto();
+        break;
+      
+      case 'J':
+        curto(); longo(); longo(); longo();
+        break;
+      
+      case 'K':
+        longo(); curto(); longo();
+        break;
+      
+      case 'L':
+        curto(); longo(); curto(); curto();
+        break;
+      
+      case 'M':
+        longo(); longo();
+        break;
+      
+      case 'N':
+        longo(); curto();
+        break;
+      
+      case 'O':
+        longo(); longo(); longo();
+        break;
+      
+      case 'P':
+        curto(); longo(); longo(); curto();
+        break;
+      
+      case 'Q':
+        longo(); longo(); curto(); longo();
+        break;
+      
+      case 'R':
+        curto(); longo(); curto();
+        break;
+      
+      case 'S':
+        curto(); curto(); curto();
+        break;
+      
+      case 'T':
+        longo();
+        break;
+      
+      case 'U':
+        curto(); curto(); longo();
+        break;
+      
+      case 'V':
+        curto(); curto(); curto(); longo();
+        break;
+      
+      case 'W':
+        curto(); longo(); longo();
+        break;
+      
+      case 'X':
+        longo(); curto(); curto(); longo();
+        break;
+      
+      case 'Y':
+        longo(); curto(); longo(); longo();
+        break;
+      
+      case 'Z':
+        longo(); longo(); curto(); curto();
+        break;
+
+      case '1':
+        curto(); longo(); longo(); longo(); longo();
+        break;
+
+      case '2':
+        curto(); curto(); longo(); longo(); longo();
+        break;
+
+      case '3':
+        curto(); curto(); curto(); longo(); longo();
+        break;
+
+      case '4':
+        curto(); curto(); curto(); curto(); longo();
+        break;
+
+      case '5':
+        curto(); curto(); curto(); curto(); curto();
+        break;
+
+      case '6':
+        longo(); curto(); curto(); curto(); curto();
+        break;
+
+      case '7':
+        longo(); longo(); curto(); curto(); curto();
+        break;
+
+      case '8':
+        longo(); longo(); longo(); curto(); curto();
+        break;
+
+      case '9':
+        longo(); longo(); longo(); longo(); curto();
+        break;
+
+      case '0':
+        longo(); longo(); longo(); longo(); longo();
+        break;
+  }
+}
+
   
-void setup() {
+void setup()
+{
   pinMode(LED, OUTPUT);
 }
 
-void loop() {
-  morse("AbC");
-  delay(10*INTERV1);
-  morse("SOS");
-  delay(10*INTERV1);
+void loop()
+{
+  morse("Ola");
+  espacoPalavras();
+  morse("Mundo");
+  espacoFrases();
 }
